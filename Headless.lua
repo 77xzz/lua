895f33b24bee601b4ec0dd183d53891f9b57c837
77xzz <133784524+77xzz@users.noreply.github.com>
2023-08-13 16:39:40 +0200

    Create Headless.lua

---
+++ Headless.lua
@@ -0,0 +1,17 @@
+local StartUpArguments = ({...})[1] or {}
+--
+local Headless = (StartUpArguments.On or false)
+--
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+--
+if LocalPlayer.Character and LocalPlayer.Character.Humanoid then
+    for i,v in pairs(LocalPlayer.Character:GetChildren()) do
+        if (v.Name == "Head" and v.ClassName == "Part") then
+            v.Transparency = (Headless and 1 or 0)
+            if v:FindFirstChild("face") then
+                v.face.Transparency = (Headless and 1 or 0)
+            end
+        end
+    end
+end
